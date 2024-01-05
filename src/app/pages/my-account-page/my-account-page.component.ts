922dc8bbbb78ceb857b7300a6f641e57a46122bd
SzymonRegula <szymon.regula@gmail.com>
2024-01-05 08:18:54 +0100

    generate main pages and components

---
+++ src/app/pages/my-account-page/my-account-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-account-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-account-page.component.html',
+  styleUrl: './my-account-page.component.scss'
+})
+export class MyAccountPageComponent {
+
+}
