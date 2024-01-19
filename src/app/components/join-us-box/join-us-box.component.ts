0e0354b67e805a18df3851d3e57a0978560cf55b
SzymonRegula <szymon.regula@gmail.com>
2024-01-19 03:07:55 +0100

    create join-us-box component

--- src/app/components/join-us-box/join-us-box.component.ts
+++ src/app/components/join-us-box/join-us-box.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
+import { RouterLink } from '@angular/router';
+import { CapitalizeFirstPipe } from '../../pipes/capitalize-first.pipe';
+import { Role } from '../../models/role.model';
 
 @Component({
   selector: 'app-join-us-box',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, RouterLink, CapitalizeFirstPipe],
   templateUrl: './join-us-box.component.html',
-  styleUrl: './join-us-box.component.scss'
+  styleUrl: './join-us-box.component.scss',
 })
-export class JoinUsBoxComponent {
+export class JoinUsBoxComponent implements OnInit {
+  @Input() role: Role = 'student';
+  @Input() description: string = '';
+  imageSrc = 'assets/join-us-student.jpeg';
 
+  ngOnInit(): void {
+    if (this.role === 'trainer') {
+      this.imageSrc = 'assets/join-us-trainer.png';
+    }
+  }
 }
