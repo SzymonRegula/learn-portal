7553ce67477a5f9de00d20c9e7371dc5ddde7abd
SzymonRegula <szymon.regula@gmail.com>
2024-01-19 03:08:56 +0100

    create table component

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { CustomDatePipe } from '../../pipes/custom-date.pipe';
+import { CapitalizeFirstPipe } from '../../pipes/capitalize-first.pipe';
+
+export type TrainingData = {
+  date: Date;
+  trainingName: string;
+  type: 'webinar';
+  name: string;
+  duration: string;
+};
+
+const ELEMENT_DATA: TrainingData[] = [
+  {
+    date: new Date('2020-01-01'),
+    trainingName: 'Training 1',
+    type: 'webinar',
+    name: 'John Doe',
+    duration: '1h',
+  },
+
+  {
+    date: new Date('2020-01-01'),
+    trainingName: 'Training 2',
+    type: 'webinar',
+    name: 'John Doe',
+    duration: '1h',
+  },
+];
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, CustomDatePipe, CapitalizeFirstPipe],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrl: './table.component.scss',
 })
 export class TableComponent {
-
+  studentView = true;
+  displayedColumns: string[] = [
+    'date',
+    'trainingName',
+    'type',
+    'name',
+    'duration',
+  ];
+  dataSource = ELEMENT_DATA;
 }
