eedf13858b3e02b4ad1931bae7ee4813ae0aac74
SzymonRegula <szymon.regula@gmail.com>
2024-01-19 03:23:05 +0100

    create login-form component

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,12 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ButtonComponent } from '../button/button.component';
+import { RouterLink } from '@angular/router';
+import { faUser, faEyeSlash } from '@fortawesome/free-regular-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RecaptchaFormsModule, RecaptchaModule } from 'ng-recaptcha';
+import { environment } from '../../../environments/environment';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    ButtonComponent,
+    RouterLink,
+    FontAwesomeModule,
+    RecaptchaModule,
+    RecaptchaFormsModule,
+    LoadingSpinnerComponent,
+  ],
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
 })
 export class LoginFormComponent {
+  userIcon = faUser;
+  lockIcon = faLock;
+  eyeIcon = faEyeSlash;
+  hidePassword = true;
+  isLoading = false;
+  siteKey = environment.recaptchaV2SiteKey;
+  captchaResponse: string | null = null;
+  private toastr = inject(ToastrService);
 
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      form.control.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
+
+    // TODO: Validate captcha with backend
+
+    // Simulate a server call
+    setTimeout(() => {
+      console.log(form.value);
+      this.isLoading = false;
+
+      this.toastr.success('Logged in successfully!');
+    }, 1000);
+  }
+
+  resolved(captchaResponse: string | null) {
+    console.log(`Resolved captcha with response: ${captchaResponse}`);
+    this.captchaResponse = captchaResponse;
+  }
 }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  recaptchaV2SiteKey: '6LeLj0wpAAAAAKsSYnJz0eTvaC4doZJ67J9enj0_',
+};
