db3f7a9436bac684bfd1d10a1a34eadad1e8d00e
SzymonRegula <szymon.regula@gmail.com>
2024-01-19 03:17:46 +0100

    create date-picker component

--- src/app/components/date-picker/date-picker.component.ts
+++ src/app/components/date-picker/date-picker.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { DateRange } from '../../models/date-range.model';
 
 @Component({
   selector: 'app-date-picker',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './date-picker.component.html',
-  styleUrl: './date-picker.component.scss'
+  styleUrl: './date-picker.component.scss',
+  providers: [],
 })
-export class DatePickerComponent {
+export class DatePickerComponent implements OnInit {
+  @Output() rangeChange = new EventEmitter<DateRange>();
 
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+
+  ngOnInit(): void {
+    this.range.valueChanges.subscribe((value) => {
+      this.rangeChange.emit(value as DateRange);
+    });
+  }
 }
