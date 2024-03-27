8853cb6dd9b70b2c8c75cf04ed3276dbfc17aaa2
Szymon Reguła <113403572+SzymonRegula@users.noreply.github.com>
2024-03-27 01:52:40 +0100

    add error handling service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,21 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideToastr } from 'ngx-toastr';
+import { tokenInterceptor } from './auth/interceptors/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(withFetch(), withInterceptors([tokenInterceptor])),
     provideAnimations(),
     provideToastr(),
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
+
   recaptchaV2SiteKey: '6LeLj0wpAAAAAKsSYnJz0eTvaC4doZJ67J9enj0_',
+  apiUrl: 'https://6038kolcw8.execute-api.eu-north-1.amazonaws.com',
 };
